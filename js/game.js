d8ad9bf074a5439126bd3d0bf1e1a704b8a299f2
vlavaka <vronlavs@gmail.com>
2020-08-14 19:35:53 -0600

    added playName to js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+var name = "your name";
+console.log(name);
+
+// this creates a function name "fight"
+
+function fight() {
+window.alert("The fight has begun");
+} 
+
+// fight()
